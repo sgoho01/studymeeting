1d9f5b9d0d449816136fe2b293937ca5a536f87a
ghsong <ghsong@gmail.com>
2020-04-04 14:02:34 +0900

    계정 도메인 추가

---
+++ src/main/java/com/ghsong/studymeeting/domain/Account.java
@@ -0,0 +1,60 @@
+package com.ghsong.studymeeting.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author : song6
+ * Date: 2020-04-04
+ * Copyright(©) 2020
+ */
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;        // varchar(255)
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+
+}
