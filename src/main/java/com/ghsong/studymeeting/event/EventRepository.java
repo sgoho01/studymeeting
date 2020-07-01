06fccd533f428ce94257e0a390ceff01f790045c
song6497 <song6497@gmail.com>
2020-07-01 15:57:59 +0900

    스프링과 JPA 기반 웹 애플리케이션 개발 강좌 (모임 만들기 폼 서브밋)

---
+++ src/main/java/com/ghsong/studymeeting/event/EventRepository.java
@@ -0,0 +1,10 @@
+package com.ghsong.studymeeting.event;
+
+import com.ghsong.studymeeting.domain.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+}
---
+++ src/main/java/com/ghsong/studymeeting/event/EventService.java
@@ -0,0 +1,26 @@
+package com.ghsong.studymeeting.event;
+
+import com.ghsong.studymeeting.domain.Account;
+import com.ghsong.studymeeting.domain.Event;
+import com.ghsong.studymeeting.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+
+    public Event createEvent(Event event, Study study, Account account) {
+        event.setCreateBy(account);
+        event.setCreateDateTime(LocalDateTime.now());
+        event.setStudy(study);
+        return eventRepository.save(event);
+    }
+}
