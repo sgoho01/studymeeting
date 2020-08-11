0720dbc3aa27f3ab84fbad4c71d4de6890c68a9b
ghsong <ghsong@greenlamp.kr>
2020-08-11 18:08:15 +0900

    스프링과 JPA 기반 웹 애플리케이션 개발 강좌 (알림 아이콘 변경)

--- src/main/java/com/ghsong/studymeeting/modules/notification/NotificationRepository.java
+++ src/main/java/com/ghsong/studymeeting/modules/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.ghsong.studymeeting.modules.notification;
 
+import com.ghsong.studymeeting.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    Long countByAccountAndChecked(Account account, boolean checked);
 }
