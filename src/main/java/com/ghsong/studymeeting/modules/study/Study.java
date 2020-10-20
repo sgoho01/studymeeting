cb8ea4e8ceb1be98f83610500bb88fd9ed5b6c0f
ghsong <ghsong@greenlamp.kr>
2020-10-20 12:08:50 +0900

    스프링과 JPA 기반 웹 애플리케이션 개발 강좌 (검색뷰 개선)

--- src/main/java/com/ghsong/studymeeting/modules/study/Study.java
+++ src/main/java/com/ghsong/studymeeting/modules/study/Study.java
@@ -71,16 +71,20 @@ public class Study {
 
     private boolean useBanner;
 
+    private int memberCount;
+
     public void addManager(Account account) {
         this.managers.add(account);
     }
 
     public void addMember(Account account) {
         this.members.add(account);
+        this.memberCount++;
     }
 
     public void removeMember(Account account) {
         this.members.remove(account);
+        this.memberCount--;
     }
 
     public boolean isJoinable(UserAccount userAccount) {
